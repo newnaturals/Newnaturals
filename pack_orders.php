46f589715c0f329d401a7a5c38c84b1ae29b06cc
Helene Wallis <41833903+newnaturals@users.noreply.github.com>
2023-05-16 10:33:25 -0400

    Add files via upload

---
+++ pack_orders.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Definitions for order packing plug in
+ * This is the file that goes in /YOUR_ADMIN/includes/language/english/extra_definitions/
+ * @copyright Helene Wallis, 5&2 Enterprises LLC (helene@newnaturalsonline.com)
+ * @copyright Portions Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ 
+ */
+
+define ('BOX_TOOLS_PACK_ORDERS', 'Pack Orders');
---
+++ pack_orders_filenames.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Add-On: Pack Orders v1.0
+ * Designed for: Zen Cart 1.5.7 and up
+ * Definitions for order packing plug in
+ * This is the file that goes in /YOUR_ADMIN/includes/extra_datafiles
+ * @copyright Helene Wallis, 5&2 Enterprises LLC (helene@newnaturalsonline.com)
+ * @copyright Portions Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ 
+ */
+ 
+ 
+// Pack Orders Filename Defines
+  define('FILENAME_PACK_ORDERS', 'pack_orders');
+//  define('FILENAME_DEFINE_PACK_ORDERS', 'define_pack_orders'); // don't know yet whether this is needed or not
+?>
